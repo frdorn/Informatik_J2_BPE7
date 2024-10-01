3eb577c775d62490757900a6941391a97ab22cbe
F Dorn <117225004+frdorn@users.noreply.github.com>
2024-10-01 10:58:57 +0200

    Update L1 2 Beispiel Array Klasse.js

--- L1 2 Beispiel Array Klasse.js
+++ L1 2 Beispiel Array Klasse.js
@@ -1,7 +1,10 @@
 var sgwgj2 = ["Victoria", "Jakub", "Franziska", "Vivien", "Alec"];
+// Bestimmen der Länge des Arrays sgwgj2 und Speichern in einer Variable
 var anzSchuelerInnen = sgwgj2.length;
+// Deklarieren und Initialisieren einer Variable, in der die Ausgaben gesammelt werden 
 var meldung = "";
 for (var i = 0; i < anzSchuelerInnen; i++) {
+  // Anfügen von Hallo und aktuellem Namen an die Variable meldung
   meldung += "Hallo " + sgwgj2[i] + "\n"
 }
 alert(meldung);
