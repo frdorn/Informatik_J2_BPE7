802c573c165ca02def03dce561734b6c43a0c4b8
F Dorn <117225004+frdorn@users.noreply.github.com>
2024-10-01 10:47:02 +0200

    Update L1 2 Beispiel Array Klasse.js

---
+++ L1 2 Beispiel Array Klasse.js
@@ -0,0 +1,7 @@
+var sgwgj2 = ["Victoria", "Jakub", "Franziska", "Vivien", "Alec"];
+var anzSchuelerInnen = sgwgj2.length;
+var meldung = "";
+for (var i = 0; i < anzSchuelerInnen; i++) {
+  meldung += "Hallo " + sgwgj2[i] + "\n"
+}
+alert(meldung);
